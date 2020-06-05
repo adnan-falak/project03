f7b261908324e42c4802c42847d8e8b70751f604
adnan-falak <adnanfalak2332@gmail.com>
2020-06-05 08:30:48 +0500

    Update App.js
    Clock Is Added

--- src/App.js
+++ src/App.js
@@ -23,5 +23,47 @@ function App() {
     </div>
   );
 }
+function showTime() {
+  var sT= new Date();
+  var h= sT.getHours();
+  var m= sT.getMinutes();
+  var s= sT.getSeconds();
+  var mer="AM";
+  	if (h>=12) {
+  		mer="PM";
+  	}
+  	if (h==0) {
+  		h=12;
+  	}
+  	if (h>12) {
+  		h=h-12;
+  	}
+  	if (h<10) {
+  		h="0"+h;
+  	}
+  	if (m<10) {
+  		m="0"+m;
+  	}
+  	if (s<10) {
+  		s="0"+s;
+  	}
+  var time=h+" "+":"+" "+m+" "+":"+" "+s;
+  document.getElementById("main").innerText=time+" "+mer;
+  setTimeout(showTime,1000);
+
+}
+showTime();
+
+function alsoDate() {
+	var date= new Date();
+	var showDate=date.toDateString();
+	document.getElementById('date').innerText=showDate;
+}
+alsoDate();
+
+function line() {
+	document.getElementById('java').innerText="Digital Clock In javascript";
+}
+line();
 
 export default App;
